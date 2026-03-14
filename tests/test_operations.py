6875995746f7503c865ea9a272a3afb53df25b2a
GaneshGani1234 <160120737119venkataganesh@gmail.com>
2026-03-15 00:34:58 +0530

    unit test cases added

---
+++ tests/test_operations.py
@@ -0,0 +1,9 @@
+from src.math_operations import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(1,4)==5
+def test_sub():
+    assert sub(3,2)==1
+    assert sub(9,4)==5
+    assert sub(1,2)==-1
